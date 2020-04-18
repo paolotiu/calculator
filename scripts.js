86af3a28f0155190ca53d68073b0cac47f797d0a
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2020-04-18 16:51:51 +0800

    display/clear numbers

--- scripts.js
+++ scripts.js
@@ -20,3 +20,13 @@ function operate (operator, a, b){
     (operator == '*') ? multiply(a,b) :
     divide(a,b)  
 }
+
+let display = document.querySelector('#result');
+let digits = document.querySelectorAll('.digits');
+digits = Array.from(digits);
+
+// adds text to display
+digits.forEach((button) => button.addEventListener('click', () => display.innerText += button.innerText))
+
+const clear = document.getElementById('clear');
+clear.onclick = () => display.innerText = '';
