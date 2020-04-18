e47bc818958c2014d434811b60599bac53ec5156
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2020-04-18 15:52:55 +0800

    added operate function

--- scripts.js
+++ scripts.js
@@ -13,3 +13,10 @@ function multiply (a,b){
 function divide (a,b){
     return a / b;
 }
+
+function operate (operator, a, b){
+    return (operator == '+') ? add(a,b) :
+    (operator == '-') ? subtract(a,b) :
+    (operator == '*') ? multiply(a,b) :
+    divide(a,b)  
+}
